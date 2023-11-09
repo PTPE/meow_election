4f0b0fdade4ba19f7657c1eeab383fee8bfee6ff
Lee Yu Chieh <winnie524722@gmail.com>
2023-11-09 20:14:14 +0800

    feat:部署github pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { AppRoutes } from "./routes/route";
-import "./index.css";
-
 import { GlobalStyle, ResetStyle } from "./styles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
